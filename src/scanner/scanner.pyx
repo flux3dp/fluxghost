fee201f74dfdebdbd1fa0b636f5f78e3ff029987
Yen <yen@flux3dp.com>
2015-06-01 11:54:38 +0800

    minor: change order

--- src/scanner/scanner.pyx
+++ src/scanner/scanner.pyx
@@ -1,4 +1,3 @@
-
 cdef extern from "noise_del.h":
     cdef cppclass PointCloudXYZRGB:
         pass
@@ -9,6 +8,31 @@ cdef extern from "noise_del.h":
 
     int SOR(PointCloudXYZRGB cloud, int neighbors, float thresh)
     int VG(PointCloudXYZRGB cloud)
+
+cdef class PointCloudXYZRGBObj:
+    cdef PointCloudXYZRGB obj
+
+    def __init__(self):
+        self.obj = createPointCloudXYZRGB()
+
+    cpdef loadFile(self, unicode filename):
+        if loadPointCloudXYZRGB(filename.encode(), self.obj) == -1:
+            raise RuntimeError("Load failed")
+
+    cpdef PointCloudXYZRGBObj clone(self):
+        cdef PointCloudXYZRGBObj obj = PointCloudXYZRGBObj()
+        # TODO:
+        raise RuntimeError("Not implement clone yet")
+        return obj
+
+    cpdef dump(self, unicode filename):
+        dumpPointCloudXYZRGB(filename.encode(), self.obj)
+
+    cpdef int SOR(self, int neighbors, float threshold):
+        return SOR(self.obj, neighbors, threshold)
+
+    cpdef int VG(self):
+        return VG(self.obj)
 
 cdef extern from "reg.h":
     cdef cppclass PointNT:
@@ -39,28 +63,3 @@ cdef class RegCloud:
             raise RuntimeError("Load failed")
         if loadPointCloudPointNormal(filename_obj.encode(), self.obj) == -1:
             raise RuntimeError("Load failed")
-
-cdef class PointCloudXYZRGBObj:
-    cdef PointCloudXYZRGB obj
-
-    def __init__(self):
-        self.obj = createPointCloudXYZRGB()
-
-    cpdef loadFile(self, unicode filename):
-        if loadPointCloudXYZRGB(filename.encode(), self.obj) == -1:
-            raise RuntimeError("Load failed")
-
-    cpdef PointCloudXYZRGBObj clone(self):
-        cdef PointCloudXYZRGBObj obj = PointCloudXYZRGBObj()
-        # TODO:
-        raise RuntimeError("Not implement clone yet")
-        return obj
-
-    cpdef dump(self, unicode filename):
-        dumpPointCloudXYZRGB(filename.encode(), self.obj)
-
-    cpdef int SOR(self, int neighbors, float threshold):
-        return SOR(self.obj, neighbors, threshold)
-
-    cpdef int VG(self):
-        return VG(self.obj)
