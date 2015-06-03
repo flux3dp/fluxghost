ef733e23d1780e3bec4f7172a30dc3e3869589cf
Yen <yen@flux3dp.com>
2015-06-03 18:20:11 +0800

    delete unsupported api(laser parser a.k.a laser_pattern)

--- fluxghost/http_websocket_route.py
+++ fluxghost/http_websocket_route.py
@@ -4,7 +4,6 @@ from fluxghost.websocket.discover import WebsocketDiscover
 from fluxghost.websocket.echo import WebsocketEcho
 from fluxghost.websocket.file import WebsocketFile
 from fluxghost.websocket.laser_bitmap_parser import WebsocketLaserBitmapParser
-from fluxghost.websocket.laser_parser import WebsocketLaserParser
 from fluxghost.websocket.scan_modeling import Websocket3DScannModeling
 from fluxghost.websocket.scan_control import Websocket3DScanControl
 
@@ -12,7 +11,6 @@ from fluxghost.websocket.scan_control import Websocket3DScanControl
 SERVICES = [WebsocketEcho,
             WebsocketFile,
             WebsocketLaserBitmapParser,
-            WebsocketLaserParser,
             Websocket3DScannModeling,
             Websocket3DScanControl,
             WebsocketControl,
