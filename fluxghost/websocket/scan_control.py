e888d8acf4af9809599efdf3ccb645e72183b613
Yen <yen@flux3dp.com>
2015-07-02 17:30:29 +0800

    fake data: scan return real world scanned data

--- fluxghost/websocket/scan_control.py
+++ fluxghost/websocket/scan_control.py
@@ -63,19 +63,19 @@ class Websocket3DScanControl(WebSocketBase):
         # <<<<<<<< Fake Code
         PCD_LOCATION = os.path.join(os.path.dirname(__file__), "..", "assets")
 
-        pc_L = read_pcd(PCD_LOCATION + '/L.pcd')
+        pc_L = read_pcd(PCD_LOCATION + '/LL.pcd')
         self.send_text('{"status": "chunk", "left": %d, "right": 0}' % len(pc_L))
         buf = []
         for p in pc_L:
-            buf.append(struct.pack('<' + 'f' * 6, *p))
+            buf.append(struct.pack('<' + 'f' * 6, p[0], p[1], p[2], p[3] / 255., p[4] / 255., p[5] / 255.))
         buf = b''.join(buf)
         self.send_binary(buf)
 
-        pc_R = read_pcd(PCD_LOCATION + '/R.pcd')
+        pc_R = read_pcd(PCD_LOCATION + '/RR.pcd')
         self.send_text('{"status": "chunk", "left": 0, "right": %d}' % len(pc_R))
         buf = []
         for p in pc_R:
-            buf.append(struct.pack('<' + 'f' * 6, *p))
+            buf.append(struct.pack('<' + 'f' * 6, p[0], p[1], p[2], p[3] / 255., p[4] / 255., p[5] / 255.))
         buf = b''.join(buf)
         self.send_binary(buf)
         # >>>>>>>> Fake Code
