344b118e848cf4dc7c39aeb0ee759d5d74eca4b1
Yen <yen@flux3dp.com>
2015-07-13 12:30:50 +0800

    fake code: cube pc

--- fluxghost/websocket/scan_control.py
+++ fluxghost/websocket/scan_control.py
@@ -70,52 +70,69 @@ class Websocket3DScanControl(WebSocketBase):
 
         self.send_text("ok")
         # <<<<<<<< Fake Code: read in a pcd file
-        # PCD_LOCATION = os.path.join(os.path.dirname(__file__), "..", "assets")
+        mode = 'cube'
+        if mode == 'pcd':
+            PCD_LOCATION = os.path.join(os.path.dirname(__file__), "..", "assets")
 
-        # pc_L = read_pcd(PCD_LOCATION + '/LL.pcd')
-        # self.send_text('{"status": "chunk", "left": %d, "right": 0}' % len(pc_L))
-        # buf = []
-        # for p in pc_L:
-        #     buf.append(struct.pack('<' + 'f' * 6, p[0], p[1], p[2], p[3] / 255., p[4] / 255., p[5] / 255.))
-        # buf = b''.join(buf)
-        # self.send_binary(buf)
-
-        # pc_R = read_pcd(PCD_LOCATION + '/RR.pcd')
-        # self.send_text('{"status": "chunk", "left": 0, "right": %d}' % len(pc_R))
-        # buf = []
-        # for p in pc_R:
-        #     buf.append(struct.pack('<' + 'f' * 6, p[0], p[1], p[2], p[3] / 255., p[4] / 255., p[5] / 255.))
-        # buf = b''.join(buf)
-        # self.send_binary(buf)
-
-        # >>>>>>>> Fake Code: generate a hemisphere
-        import math
-        STEPS = 200
-
-        step = math.pi * 2 / STEPS
-        for i in range(STEPS):
-            r = step * i
-
-            try:
-                c = math.cos(r) / math.sin(r)
-            except ZeroDivisionError:
-                c = float("INF")
-
-            self.send_text('{"status": "chunk", "left": 100, "right": 100}')
-
-            buf = b""
-            for iz in range(-100, 100):
-                z = iz / 200
-                x = math.sqrt((1 - (z ** 2)) / (1 + c ** 2))
-
-                if step > math.pi / 2 and step < (math.pi * 3 / 4):
-                    x = -x
-
-                y = x * c if c != float("INF") else 1
-
-                buf += struct.pack("<ffffff", x, y, z, 1.0, 1.0, 1.0)
+            pc_L = read_pcd(PCD_LOCATION + '/LL.pcd')
+            self.send_text('{"status": "chunk", "left": %d, "right": 0}' % len(pc_L))
+            buf = []
+            for p in pc_L:
+                buf.append(struct.pack('<' + 'f' * 6, p[0], p[1], p[2], p[3] / 255., p[4] / 255., p[5] / 255.))
+            buf = b''.join(buf)
             self.send_binary(buf)
 
+            pc_R = read_pcd(PCD_LOCATION + '/RR.pcd')
+            self.send_text('{"status": "chunk", "left": 0, "right": %d}' % len(pc_R))
+            buf = []
+            for p in pc_R:
+                buf.append(struct.pack('<' + 'f' * 6, p[0], p[1], p[2], p[3] / 255., p[4] / 255., p[5] / 255.))
+            buf = b''.join(buf)
+            self.send_binary(buf)
+
+        # >>>>>>>> Fake Code: generate a hemisphere
+        elif mode == 'hemisphere':
+            import math
+            STEPS = 200
+
+            step = math.pi * 2 / STEPS
+            for i in range(STEPS):
+                r = step * i
+
+                try:
+                    c = math.cos(r) / math.sin(r)
+                except ZeroDivisionError:
+                    c = float("INF")
+
+                self.send_text('{"status": "chunk", "left": 100, "right": 100}')
+
+                buf = b""
+                for iz in range(-100, 100):
+                    z = iz / 200
+                    x = math.sqrt((1 - (z ** 2)) / (1 + c ** 2))
+
+                    if step > math.pi / 2 and step < (math.pi * 3 / 4):
+                        x = -x
+
+                    y = x * c if c != float("INF") else 1
+
+                    buf += struct.pack("<ffffff", x, y, z, 1.0, 1.0, 1.0)
+                self.send_binary(buf)
+
+        # >>>>>>>> Fake Code
+        # >>>>>>>> Fake Code: generate cube point cloud
+        elif mode == 'cube':
+            import math
+            STEPS = 200
+            for i in range(STEPS):
+                buf = []
+                self.send_text('{"status": "chunk", "left": 1250, "right": 1250}')
+                for j in range(-50, 50, 2):
+                    for k in range(-50, 50, 2):
+                        x, y, z = map(float, [j, k, i / 2.])
+                        buf.append(struct.pack("<ffffff", x, y, z, 0.0, 0.0, 0.0))
+                buf = b"".join(buf)
+                self.send_binary(buf)
         # >>>>>>>> Fake Code
 
         self.send_text("finished")
