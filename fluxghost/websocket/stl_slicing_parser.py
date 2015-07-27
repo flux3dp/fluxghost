059c205b52c06cfce7a6092a97f7766bd00d1fe6
Yen <yen@flux3dp.com>
2015-07-27 23:35:44 +0800

    minor: return type of generate_gcode should be str

--- fluxghost/websocket/stl_slicing_parser.py
+++ fluxghost/websocket/stl_slicing_parser.py
@@ -89,9 +89,9 @@ class Websocket3DSlicing(WebsocketBinaryHelperMixin, WebSocketBase):
 
     def generate_gcode(self, params):
         names = params.split(' ')
-        gcode_buf, metadata = self.m_stl_slicer.generate_gcode(names)
-        self.send_text('{status: "complete", length: %d, time: %.3f, filament_length: %.2f}' % (len(gcode_buf), metadata[0], metadata[1]))
-        self.send_binary(gcode_buf)
+        gcode, metadata = self.m_stl_slicer.generate_gcode(names)
+        self.send_text('{status: "complete", length: %d, time: %.3f, filament_length: %.2f}' % (len(gcode), metadata[0], metadata[1]))
+        self.send_binary(gcode.encode())
 
     def delete(self, params):
         name = params.rstrip()
