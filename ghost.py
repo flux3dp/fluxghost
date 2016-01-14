bc36d4ba5fc26852cddcad7b629d6ac271f3a838
Yen <yen@flux3dp.com>
2016-01-14 17:40:21 +0800

    put parsing argument code into main()

--- ghost.py
+++ ghost.py
@@ -71,55 +71,59 @@ def setup_logger(options):
     })
 
 
-parser = argparse.ArgumentParser(description='FLUX Ghost')
-parser.add_argument("--assets", dest='assets', type=str,
-                    default=None, help="Assets folder")
-parser.add_argument("--ip", dest='ipaddr', type=str, default='127.0.0.1',
-                    help="Bind to IP Address")
-parser.add_argument("--port", dest='port', type=int, default=8000,
-                    help="Port")
-parser.add_argument("--log", dest='logfile', type=str, default=None,
-                    help="Output log to specific")
-parser.add_argument('-d', '--debug', dest='debug', action='store_const',
-                    const=True, default=False, help='Enable debug')
-parser.add_argument('-s', '--simulate', dest='simulate', action='store_const',
-                    const=True, default=False, help='Simulate data')
-parser.add_argument("--slic3r", dest='slic3r', type=str, default='../Slic3r/slic3r.pl',
-                    help="Set slic3r location")
-parser.add_argument("--sentry", dest='sentry', type=str, default=None,
-                    help="Use sentry logger")
-parser.add_argument('--test', dest='test', action='store_const',
-                    const=True, default=False, help='Run test')
+def main():
+    parser = argparse.ArgumentParser(description='FLUX Ghost')
+    parser.add_argument("--assets", dest='assets', type=str,
+                        default=None, help="Assets folder")
+    parser.add_argument("--ip", dest='ipaddr', type=str, default='127.0.0.1',
+                        help="Bind to IP Address")
+    parser.add_argument("--port", dest='port', type=int, default=8000,
+                        help="Port")
+    parser.add_argument("--log", dest='logfile', type=str, default=None,
+                        help="Output log to specific")
+    parser.add_argument('-d', '--debug', dest='debug', action='store_const',
+                        const=True, default=False, help='Enable debug')
+    parser.add_argument('-s', '--simulate', dest='simulate', action='store_const',
+                        const=True, default=False, help='Simulate data')
+    parser.add_argument("--slic3r", dest='slic3r', type=str, default='../Slic3r/slic3r.pl',
+                        help="Set slic3r location")
+    parser.add_argument("--sentry", dest='sentry', type=str, default=None,
+                        help="Use sentry logger")
+    parser.add_argument('--test', dest='test', action='store_const',
+                        const=True, default=False, help='Run test')
 
-options = parser.parse_args()
-setup_logger(options)
+    options = parser.parse_args()
+    setup_logger(options)
 
-if options.debug:
-    os.environ["flux_debug"] = "1"
-    from fluxghost.http_server_debug import HttpServer
-else:
-    from fluxghost.http_server import HttpServer
+    if options.debug:
+        os.environ["flux_debug"] = "1"
+        from fluxghost.http_server_debug import HttpServer
+    else:
+        from fluxghost.http_server import HttpServer
 
-if options.test:
-    from tests.main import main
+    if options.test:
+        from tests.main import main
+        main()
+        sys.exit(0)
+
+    if options.simulate:
+        os.environ["flux_simulate"] = "1"
+
+    if options.slic3r:
+        os.environ["slic3r"] = options.slic3r
+
+    if not options.assets:
+        options.assets = os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            "fluxghost", "assets")
+
+    server = HttpServer(assets_path=options.assets,
+                        enable_discover=True,
+                        address=(options.ipaddr, options.port,),)
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
     main()
-    sys.exit(0)
-
-if options.simulate:
-    os.environ["flux_simulate"] = "1"
-
-if options.slic3r:
-    os.environ["slic3r"] = options.slic3r
-
-
-if not options.assets:
-    options.assets = os.path.join(
-        os.path.dirname(
-            os.path.abspath(__file__)),
-        "fluxghost", "assets")
-
-server = HttpServer(assets_path=options.assets,
-                    enable_discover=True,
-                    address=(options.ipaddr, options.port,),)
-
-server.serve_forever()
