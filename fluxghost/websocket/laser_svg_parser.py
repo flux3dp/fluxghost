f3f7674fc59b9de45fb4744fe18e18cad0f06402
Yagami Cerberus <yagami-cerberus@users.noreply.github.com>
2016-03-04 16:38:37 +0800

    Revert commit 3930895

--- fluxghost/websocket/laser_svg_parser.py
+++ fluxghost/websocket/laser_svg_parser.py
@@ -47,12 +47,7 @@ class WebsocketLaserSvgParser(OnTextMessageMixin, WebsocketBinaryHelperMixin, We
         if args[0] == 'upload':
             logger.debug("upload name:%s" % (name))
             try:
-                warning, content = self.m_laser_svg.preprocess(buf)
-                print(warning)
-                if warning:
-                    for w in warning:
-                        self.send_error(w)
-                self.m_laser_svg.svgs[name] = content
+                self.m_laser_svg.svgs[name] = self.m_laser_svg.preprocess(buf)
             except:
                 print(sys.exc_info(), file=sys.stderr)
                 # sys.exc_info()[2].print_exception(file=sys.stderr)
