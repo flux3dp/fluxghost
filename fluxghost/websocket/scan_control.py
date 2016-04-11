f5f3d25b17b1ed9b0525512c1a3fd06f20f8ae65
Yagami Cerberus <yagami-cerberus@users.noreply.github.com>
2016-04-11 17:07:23 +0800

    Scan-control: bugfix

--- fluxghost/websocket/scan_control.py
+++ fluxghost/websocket/scan_control.py
@@ -43,7 +43,6 @@ class Websocket3DScanControl(WebsocketControlBase):
 
     def __init__(self, *args, **kw):
         WebsocketControlBase.__init__(self, *args, **kw)
-        self.try_control()
         self.scan_settings = ScanSetting()
         self.cab = None
 
@@ -54,6 +53,9 @@ class Websocket3DScanControl(WebsocketControlBase):
     def on_binary_message(self, buf):
         self.text_send("Protocol error")
         self.close()
+
+    def on_connected(self):
+        self.try_control()
 
     def on_command(self, message):
         if message == "take_control":
