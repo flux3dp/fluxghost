52f54be3229140434a355b517c20c57b8b7d6209
Yagami Cerberus <yagami-cerberus@users.noreply.github.com>
2016-07-01 15:22:34 +0800

    Move shared launch code to stdalone module

--- ghost.py
+++ ghost.py
@@ -1,83 +1,10 @@
 #!/usr/bin/env python3
 
-from __future__ import absolute_import
-
-import logging.config
 import argparse
-import logging
 import sys
 import os
 
-from fluxclient.utils.version import StrictVersion
-
-
-def check_fluxclient():
-    from fluxclient import __version__ as v
-    sys.modules.pop("fluxclient")
-    lower_bound = '0.9a1'
-    upper_bound = '1.0a1'
-    if StrictVersion(v) < StrictVersion(lower_bound):
-        raise RuntimeError(
-            "Your fluxclient need to update (>={})".format(lower_bound))
-    if StrictVersion(v) > StrictVersion(upper_bound):
-        raise RuntimeError("fluxclient is too new (<{})".format(upper_bound))
-
-
-def show_version(verbose):
-    from fluxghost import __version__ as gfv
-    print("fluxghost %s" % gfv)
-    from fluxclient import __version__ as cfv
-    print("fluxclient %s" % cfv)
-
-
-def setup_logger(options):
-    log_datefmt = "%Y-%m-%d %H:%M:%S"
-    log_format = "[%(asctime)s,%(levelname)s,%(name)s] %(message)s"
-
-    log_level = logging.DEBUG if options.debug else logging.INFO
-
-    handlers = {}
-    # if sys.stdout.isatty():
-    handlers['console'] = {
-        'level': log_level,
-        'formatter': 'default',
-        'class': 'logging.StreamHandler',
-    }
-
-    if options.logfile:
-        handlers['file'] = {
-            'level': log_level,
-            'formatter': 'default',
-            'class': 'logging.handlers.RotatingFileHandler',
-            'filename': options.logfile,
-            'maxBytes': 5 * (1 << 20),  # 10M
-            'backupCount': 1
-        }
-
-    if options.sentry:
-        handlers['sentry'] = {
-            'level': 'ERROR',
-            'class': 'raven.handlers.logging.SentryHandler',
-            'dsn': options.sentry,
-        }
-
-    logging.config.dictConfig({
-        'version': 1,
-        'disable_existing_loggers': True,
-        'formatters': {
-            'default': {
-                'format': log_format,
-                'datefmt': log_datefmt
-            }
-        },
-        'handlers': handlers,
-        'loggers': {},
-        'root': {
-            'handlers': list(handlers.keys()),
-            'level': 'DEBUG',
-            'propagate': True
-        }
-    })
+from fluxghost.launcher import setup_env, show_version
 
 
 def main():
@@ -110,13 +37,12 @@ def main():
                         const=True, default=False, help='Show version')
 
     options = parser.parse_args()
-    setup_logger(options)
-
     if options.version:
         show_version(options.debug)
         sys.exit(0)
 
-    check_fluxclient()
+    setup_env(options)
+
     if options.debug:
         os.environ["flux_debug"] = "1"
 
