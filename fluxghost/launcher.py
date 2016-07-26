0aa3734f8a939e0f9028277745dc4c8b78032183
Yen <yen@flux3dp.com>
2016-07-26 11:54:41 +0800

    change lower_bound and upper_bound of fluxclient

--- fluxghost/launcher.py
+++ fluxghost/launcher.py
@@ -9,12 +9,12 @@ from fluxclient.utils.version import StrictVersion
 def check_fluxclient():
     from fluxclient import __version__ as v
     sys.modules.pop("fluxclient")
-    lower_bound = '0.9a1'
-    upper_bound = '1.0a1'
-    if StrictVersion(v) < StrictVersion(lower_bound):
+    lower_bound = '1.0a1'
+    upper_bound = '2.0a1'
+    if StrictVersion(v) <= StrictVersion(lower_bound):
         raise RuntimeError(
             "Your fluxclient need to update (>={})".format(lower_bound))
-    if StrictVersion(v) > StrictVersion(upper_bound):
+    if StrictVersion(v) >= StrictVersion(upper_bound):
         raise RuntimeError("fluxclient is too new (<{})".format(upper_bound))
 
 
