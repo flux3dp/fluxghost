bd6c68aaa29f9dfeba0217bb01bcfaabbf6e7454
yagami.cerberus <yagami-cerberus@users.noreply.github.com>
2017-02-21 15:54:15 +0800

    Add toolhead heater control in playing

--- fluxghost/api/control.py
+++ fluxghost/api/control.py
@@ -113,6 +113,7 @@ def control_api_mixin(cls):
                     "toolhead": {
                         "operation": self.set_toolhead_operating,
                         "standby": self.set_toolhead_standby,
+                        "heater": self.set_toolhead_heater,
                     },
                     "load_filament": self.load_filamend_in_play,
                     "unload_filament": self.unload_filamend_in_play,
@@ -418,6 +419,10 @@ def control_api_mixin(cls):
 
         def set_toolhead_standby(self):
             self.robot.set_toolhead_standby_in_play()
+            self.send_ok()
+
+        def set_toolhead_heater(self, index, temp):
+            self.robot.set_toolhead_heater_in_play(float(temp), int(index))
             self.send_ok()
 
         def load_filamend_in_play(self, index):
