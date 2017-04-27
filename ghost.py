d4e233b4ada4203eac01f3b0650d65110292ac4a
yagami.cerberus <yagami-cerberus@users.noreply.github.com>
2017-04-27 13:24:48 +0800

    Fix trace pid

--- ghost.py
+++ ghost.py
@@ -9,6 +9,57 @@ import os
 from fluxghost.launcher import setup_env, show_version
 
 
+def trace_pid(pid):
+    def _nt():
+        try:
+            import ctypes
+            kernel32 = ctypes.windll.kernel32
+            DWORD = ctypes.c_ulong  # noqa
+            DWORD_PTR = ctypes.POINTER(DWORD)  # noqa
+            STILL_ALIVE = 259  # noqa
+
+            h_process = kernel32.OpenProcess(1040, 0, pid)
+            if not h_process:
+                raise RuntimeError("Trace pid error, pid not exist or access deny.")
+
+            exitcode = DWORD()
+
+            while True:
+                ret = kernel32.GetExitCodeProcess(h_process, ctypes.byref(exitcode))
+                if not ret:
+                    raise RuntimeError("Trace pid error, errno=%s" % kernel32.GetLastError())
+                elif exitcode.value == 259:
+                    sleep(0.8)
+                else:
+                    break
+        except Exception as e:
+            sys.stderr.write("%s\n" % e)
+        finally:
+            os.kill(os.getpid(), SIGTERM)
+
+    def _posix():
+        try:
+            while True:
+                os.kill(pid, 0)
+                sleep(0.8)
+        except:
+            pass
+        finally:
+            os.kill(os.getpid(), SIGTERM)
+
+    from threading import Thread
+    if os.name == "posix":
+        fn = _posix
+    elif os.name == "nt":
+        fn = _nt
+    else:
+        raise RuntimeError("Unknown os.name %r" % os.name)
+
+    t = Thread(target=fn)
+    t.daemon = True
+    t.start()
+
+
 def main():
     parser = argparse.ArgumentParser(description='FLUX Ghost')
     parser.add_argument("--assets", dest='assets', type=str,
@@ -82,19 +133,7 @@ def main():
                         debug=options.debug)
 
     if options.trace_pid:
-        def r():
-            try:
-                while True:
-                    os.kill(options.trace_pid, 0)
-                    sleep(0.8)
-            except:
-                pass
-            finally:
-                os.kill(os.getpid(), SIGTERM)
-        from threading import Thread
-        t = Thread(target=r)
-        t.daemon = True
-        t.start()
+        trace_pid(options.trace_pid)
 
     server.serve_forever()
 
