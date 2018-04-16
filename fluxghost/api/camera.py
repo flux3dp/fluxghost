479b0afa188bd475b8b32265c2ebabd73727f293
Jeff Chen <jeffontheground@gmail.com>
2018-04-16 21:06:00 +0800

    Fix preview mode picture mistaken

--- fluxghost/api/camera.py
+++ fluxghost/api/camera.py
@@ -36,16 +36,14 @@ def camera_api_mixin(cls):
 
         def on_connected(self):
             if self.remote_version > CRITICAL_VERSION:
-                logger.debug("Request streaming")
-                self.robot.enable_streaming() # TODO: we should enable streaming by command not by default
             self.rlist.append(CameraWrapper(self, self.robot))
         
         def on_command(self, message):
-            # if message == 'enable_streaming':
-            #     self.robot.enable_streaming() # TODO: we should enable streaming by command not by default
-            # if message == 'require_frame':
-            #     self.robot.require_frame()
             logger.info(message)
+            if message == 'enable_streaming':
+                self.robot.enable_streaming()
+            if message == 'require_frame':
+                self.robot.require_frame()
 
         def on_image(self, camera, image):
             self.send_binary(image)
