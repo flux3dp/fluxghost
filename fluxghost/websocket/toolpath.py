5cbf259e9caad38ea251affa13bb58c68c7a8b3a
Dean sung <a71124725@gmail.com>
2020-05-08 14:31:54 +0800

    Add some logger for debug

--- fluxghost/websocket/toolpath.py
+++ fluxghost/websocket/toolpath.py
@@ -1,6 +1,13 @@
 
+import logging
+
+logger = logging.getLogger('websocket.toolpath')
+
+logger.info('Importing bitmap_toolpath')
 from fluxghost.api.bitmap_toolpath import laser_bitmap_api_mixin
+logger.info('Importing svgeditor_toolpath')
 from fluxghost.api.svgeditor_toolpath import laser_svgeditor_api_mixin
+logger.info('Importing svg_toolpath')
 from fluxghost.api.svg_toolpath import (laser_svg_api_mixin,
                                         drawing_svg_api_mixin,
                                         vinyl_svg_api_mixin)
