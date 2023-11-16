66a47066d37ff28ba9fe8497b0e1ceaeeb3be16d
Dean Sung <dean@flux3dp.com>
2023-11-16 18:40:43 +0800

    Update pack-for-firmware

--- tools/pack-for-firmware.sh
+++ tools/pack-for-firmware.sh
@@ -2,4 +2,4 @@
 #cd to script folder
 cd $(dirname $0)
 cd ../..
-tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/"  --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".vscode/" ./fluxghost
+tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/"  --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".vscode/" --exclude="*.pyc" --exclude=".DS_Store" ./fluxghost
