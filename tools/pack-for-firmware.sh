c97291d53758f3c4dfdf16d9a6dc0a1786da0c7b
Dean Sung <dean@flux3dp.com>
2024-05-13 17:47:16 +0800

    Update pack-for-firmware.sh

--- tools/pack-for-firmware.sh
+++ tools/pack-for-firmware.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 #cd to script folder
 cd $(dirname $0)
-cd ../..
+cd ..
+git stash -u
+python3 ./tools/type_hint_remover.py fluxghost
+cd ..
 tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/"  --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".vscode/" --exclude="*.pyc" --exclude=".DS_Store" ./fluxghost
+cd fluxghost
+git reset --hard
+git stash pop
