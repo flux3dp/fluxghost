b983d36560404d866d1110b379a25de355d32465
Dean Sung <dean@flux3dp.com>
2024-07-23 18:46:39 +0800

    Add get_similar_contours api (#81)

--- fluxghost/api/utils.py
+++ fluxghost/api/utils.py
@@ -6,9 +6,11 @@ import shutil
 import subprocess
 import tempfile
 
-
+import cv2
+import numpy as np
 from PIL import Image, ImageCms
 
+from fluxghost.utils.contour import find_similar_contours
 from .misc import BinaryUploadHelper, BinaryHelperMixin, OnTextMessageMixin
 
 logger = logging.getLogger('API.UTILS')
@@ -26,6 +28,7 @@ def utils_api_mixin(cls):
                 'check_exist': [self.cmd_check_exist],
                 'rgb_to_cmyk': [self.rgb_to_cmyk],
                 'split_color': [self.split_color],
+                'get_similar_contours': [self.get_similar_contours],
             }
 
         def cmd_pdf2svg(self, params):
@@ -165,5 +168,24 @@ def utils_api_mixin(cls):
             helper = BinaryUploadHelper(int(file_length), upload_callback)
             self.set_binary_helper(helper)
             self.send_json(status='continue')
+
+        def get_similar_contours(self, params):
+            params = params.split(' ')
+
+            def upload_callback(buf):
+                try:
+                    image = Image.open(io.BytesIO(buf))
+                    cv_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+                    data = find_similar_contours(cv_img)
+                    self.send_ok(data=data)
+                except Exception as e:
+                    self.send_json(status='error', info=str(e))
+
+
+            file_length = int(params[0])
+            helper = BinaryUploadHelper(int(file_length), upload_callback)
+            self.set_binary_helper(helper)
+            self.send_json(status='continue')
+
 
     return UtilsApi
---
+++ fluxghost/utils/contour/__init__.py
@@ -0,0 +1,34 @@
+import logging
+
+from .find_contours import get_contour_by_canny, get_contour_by_hsv_gradient
+from .group_contours import group_similar_contours
+from .contour_info import get_contour_info
+
+
+logger = logging.getLogger(__name__)
+
+
+def find_similar_contours(img):
+    contours = []
+    contours += get_contour_by_canny(img.copy())
+    contours += get_contour_by_hsv_gradient(img.copy())
+
+    if len(contours) == 0:
+        return []
+
+    groups = group_similar_contours(contours)
+    groups = sorted(groups, key=lambda x: len(x[0]), reverse=True)
+    logger.info('Result group number: %d' % len(groups))
+    if len(groups) == 0:
+        return []
+    group = groups[0]
+    contours = group[0]
+    logger.info('Most commen group contours: %d' % len(contours))
+
+    group_img = img.copy()
+    data = []
+    for contour in contours:
+        import cv2
+        cv2.drawContours(group_img, [contour], -1, (0, 255, 0), thickness=2)
+        data.append(get_contour_info(contour))
+    return data
