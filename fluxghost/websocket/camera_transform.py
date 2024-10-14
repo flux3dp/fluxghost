680f9a52183ba348e44c722c737e8953360206d4
Dean Sung <dean@flux3dp.com>
2024-10-14 17:46:47 +0800

    Add WebsocketCameraTransform

---
+++ fluxghost/websocket/camera_transform.py
@@ -0,0 +1,21 @@
+
+from fluxghost.api.camera_transform import camera_transform_api_mixin
+from .base import WebSocketBase
+
+
+"""
+
+Javascript Example:
+
+ws = new WebSocket("ws://127.0.0.1:8000/ws/camera_transform");
+ws.onmessage = function(v) { console.log(v.data);}
+ws.onclose = function(v) { console.log("CONNECTION CLOSED, code=" + v.code +
+    "; reason=" + v.reason); }
+
+// After recive connected...
+ws.send("ls")
+"""
+
+
+class WebsocketCameraTransform(camera_transform_api_mixin(WebSocketBase)):
+    pass
