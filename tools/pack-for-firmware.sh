babe6229d3d602fed83ec50b10e3581dad8f5ee1
Dean Sung <dean@flux3dp.com>
2025-06-16 14:15:05 +0800

    Update pack-for-firmware

--- tools/pack-for-firmware.sh
+++ tools/pack-for-firmware.sh
@@ -5,7 +5,7 @@ cd ..
 git stash -u
 python3 ./tools/type_hint_remover.py fluxghost
 cd ..
-tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/"  --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".vscode/" --exclude="*.pyc" --exclude=".DS_Store" ./fluxghost
+tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/"  --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".vscode/" --exclude=".venv/"  --exclude="*.pyc" --exclude=".DS_Store" ./fluxghost
 cd fluxghost
 git reset --hard
 git stash pop
