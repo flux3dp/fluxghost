21f81326ef458d0d29e7b5f5808e401aaa963595
Dean Sung <dean@flux3dp.com>
2026-05-13 19:13:11 +0800

    Update pack for firmware

--- tools/pack-for-firmware.sh
+++ tools/pack-for-firmware.sh
@@ -2,10 +2,17 @@
 #cd to script folder
 cd $(dirname $0)
 cd ..
-git stash -u
+rm -rf ./fluxghost.tar.gz
+STASHED=false
+if ! git diff --quiet || ! git diff --cached --quiet || [ -n "$(git ls-files --others --exclude-standard)" ]; then
+  git stash -u
+  STASHED=true
+fi
 uv run ./tools/type_hint_remover.py fluxghost
 cd ..
-tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/"  --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".vscode/" --exclude=".venv/"  --exclude="*.pyc" --exclude=".DS_Store" ./fluxghost
+tar -zcvf ./fluxghost/fluxghost.tar.gz --exclude="__pycache__" --exclude="lib/" --exclude="tools/" --exclude="dist/" --exclude="build/" --exclude="debug-imgs/" --exclude="tests/" --exclude="test-file/" --exclude="node_modules/" --exclude="*.yml" --exclude="*.spec" --exclude=".git/" --exclude=".github/" --exclude=".vscode/" --exclude=".venv/" --exclude=".claude"  --exclude="*.pyc" --exclude=".DS_Store" --exclude=".certs/" ./fluxghost
 cd fluxghost
 git reset --hard
-git stash pop
+if [ "$STASHED" = true ]; then
+  git stash pop
+fi
